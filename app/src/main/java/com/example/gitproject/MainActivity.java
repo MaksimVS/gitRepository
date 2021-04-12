44af23d20bde026fce3010a01ef9a103d5e7207d
MAXIMUS <VdovinM.S@mail.ru>
2021-04-12 19:56:09 +0700

    Implemented very important feature.

--- app/src/main/java/com/example/gitproject/MainActivity.java
+++ app/src/main/java/com/example/gitproject/MainActivity.java
@@ -3,12 +3,43 @@ package com.example.gitproject;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.EditText;
+import android.widget.Switch;
+import android.widget.TextView;
+import android.widget.ToggleButton;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_main_task_one);
+
+        TextView outputOne = findViewById(R.id.output_text_one);
+        EditText inputNumber = findViewById(R.id.input_number);
+        Button btnPay = findViewById(R.id.btn_pay);
+        TextView outputTwo = findViewById(R.id.output_text_Two);
+        CheckBox checkBox = findViewById(R.id.check_mark);
+        Switch switchOne = findViewById(R.id.switch_one);
+        ToggleButton btnExit = findViewById(R.id.btn_exit);
+
+        btnPay.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                try {
+                    Integer numberSum = Integer.valueOf(inputNumber.getText().toString());
+                    if (numberSum > 0) {
+                        outputTwo.setText("Поставьте галочку и перейдите на следующую страницу");
+                    } else {
+                        outputTwo.setText("Деньги положительными циферками пишутся!:) ");
+                    }
+                } catch (NumberFormatException exception) {
+                    outputTwo.setText("\"Деньги положительными ЦИФЕРКАМИ пишутся!:) \"");
+                }
+            }
+        });
     }
 }
